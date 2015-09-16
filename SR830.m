93b739e62fb2fd7b5130b4b405227b153de13b3f
Squidward <dhl88@cornell.edu>
2015-09-16 19:52:00 -0400

    Kevin and David wrote Agilent33250A and tester.  Tested the Agilent meter from ccmr lending library.  output confirmed at 30,000.1 Hz with oscope.
    Kevin's tuning fork code for data at 2015 09 10 - 2015 09 15.
    
    Kevin modified SR830 for setting and getting phase, amplitude, frequency, on SR830 but frequency steps ~4.5 sig figs, insufficient for tuning fork.
    
    David added commented out lookup tables for SR830 sensitivity and time getters and setters but remains unfinished.

--- SR830.m
+++ SR830.m
@@ -1,5 +1,5 @@
 %  Keithley2400 Matlab Drivers 
-%  Copyright (C) 2015 David Low, Nowack Lab
+%  Copyright (C) 2015 David Low, Kevin Nangoi, Nowack Lab
 %
 %   This program is free software: you can redistribute it and/or modify
 %   it under the terms of the GNU General Public License as published by
@@ -103,43 +103,7 @@ function x = snapshot(this)
     end
 end
 
-function sensitivity(this, level)
-    %level integer and their meanings:
-    %0  2   nV or fA
-    %1  5   nV or fA
-    %2  10  nV or fA
-    %3  20  nV or fA
-    %4  50  nV or fA
-    %5  100 nV or fA
-    %6  200 nV or fA
-    %7  500 nV or fA
-    %8  1   uV or pA
-    %9  2   uV or pA
-    %10 5   uV or pA
-    %11 10  uV or pA
-    %12 20  uV or pA
-    %13 50  uV or pA
-    %14 100 uV or pA
-    %15 200 uV or pA
-    %16 500 uV or pA
-    %17 1   mV or nA
-    %18 2   mV or nA
-    %19 5   mV or nA
-    %20 10  mV or nA
-    %21 20  mV or nA
-    %22 50  mV or nA
-    %23 100 mV or nA
-    %24 200 mV or nA
-    %25 500 mV or nA
-    %26 1    V or uA
-    try
-        fprintf(this.visa, ['SENS ',num2str(level),'\n']);
-    catch ME
-        fprintf('SR830.sensitivity: Cannot write to SR830?\n')
-        this.close();
-        rethrow(ME);
-    end
-end
+
 
 
 %%%%%%% Helper Methods
@@ -155,7 +119,7 @@ function init(this)
 
     try
         fprintf(this.visa, 'OUTX1\n'); 
-        fprintf(this.visa, '*RST\n');  %initialize lockin
+        %fprintf(this.visa, '*RST\n');  %initialize lockin
         fprintf(this.visa, 'FAST0\n'); %disables fast 
         fprintf(this.visa, 'REST\n');  %clears data buffer
     catch ME
@@ -176,82 +140,227 @@ function getparams(this)
 %(matlab_measure/scanning/getparameters_SR830.m)
 %has try catch safety with clean exit
 
-try
-    fprintf(this.visa, '*IDN?\n');
-    this.par.id = fscanf(this.visa);
-            
-    fprintf(this.visa, 'PHAS?\n');
-    this.par.phase = fscanf(this.visa);
-                    
-    fprintf(this.visa, 'FMOD?\n');
-    this.par.internal_ref = fscanf(this.visa);
-                            
-    fprintf(this.visa, 'FREQ?\n');
-    this.par.freq = fscanf(this.visa, '%f');
-                                    
-    fprintf(this.visa, 'HARM?\n');
-    this.par.harmonic = fscanf(this.visa, '%i');
-                                            
-    fprintf(this.visa, 'SLVL?\n');
-    this.par.vOutRMS = fscanf(this.visa, '%f');
-                                                   
-    fprintf(this.visa, 'ISRC?\n');
-    this.par.in_config = fscanf(this.visa, '%i');
-                                                            
-    fprintf(this.visa, 'IGND?\n');
-    this.par.in_shield_gnd = fscanf(this.visa, '%i');
-                                                                    
-    fprintf(this.visa, 'ICPL?\n');
-    this.par.in_coup_dc = fscanf(this.visa, '%i');
-                                                                            
-    fprintf(this.visa, 'ILIN?\n');
-    this.par.in_notch = fscanf(this.visa, '%i');
-    
-    fprintf(this.visa, 'SENS?\n');
-    i = fscanf(this.visa, '%i');
+	try
+		fprintf(this.visa, 'PHAS?\n');
+		this.par.phase = fscanf(this.visa);
+						
+		fprintf(this.visa, 'FMOD?\n');
+		this.par.internal_ref = fscanf(this.visa);
+								
+		fprintf(this.visa, 'FREQ?\n');
+		this.par.freq = fscanf(this.visa, '%f');
+										
+		fprintf(this.visa, 'HARM?\n');
+		this.par.harmonic = fscanf(this.visa, '%i');
+												
+		fprintf(this.visa, 'SLVL?\n');
+		this.par.vOutRMS = fscanf(this.visa, '%f');
+													   
+		fprintf(this.visa, 'ISRC?\n');
+		this.par.in_config = fscanf(this.visa, '%i');
+																
+		fprintf(this.visa, 'IGND?\n');
+		this.par.in_shield_gnd = fscanf(this.visa, '%i');
+																		
+		fprintf(this.visa, 'ICPL?\n');
+		this.par.in_coup_dc = fscanf(this.visa, '%i');
+																				
+		fprintf(this.visa, 'ILIN?\n');
+		this.par.in_notch = fscanf(this.visa, '%i');
+		
+		fprintf(this.visa, 'SENS?\n');
+		i = fscanf(this.visa, '%i');
 
-    switch rem(i, 3)
-    case 0
-         this.par.sens = 2;
-    case 1 
-         this.par.sens = 5;
-    case 2
-         this.par.sens = 10; 
-    end;
+		switch rem(i, 3)
+		case 0
+			 this.par.sens = 2;
+		case 1 
+			 this.par.sens = 5;
+		case 2
+			 this.par.sens = 10; 
+		end;
 
-    this.par.sens = this.par.sens * 10^(fix(i/3)-9);
-    
-    fprintf(this.visa, 'OEXP? 1\n');
-    this.par.oexp = fscanf(this.visa);
-    
-    fprintf(this.visa, 'RMOD?\n');
-    this.par.reserve = fscanf(this.visa);
-    
-    fprintf(this.visa, 'OFLT?\n');
-    i = fscanf(this.visa, '%i');
-    switch rem(i, 2)
-    case 0
-         this.par.timeconst = 1;
-    case 1 
-         this.par.timeconst = 3;
-    end;
+		this.par.sens = this.par.sens * 10^(fix(i/3)-9);
+		
+		fprintf(this.visa, 'OEXP? 1\n');
+		this.par.oexp = fscanf(this.visa);
+		
+		fprintf(this.visa, 'RMOD?\n');
+		this.par.reserve = fscanf(this.visa);
+		
+		fprintf(this.visa, 'OFLT?\n');
+		i = fscanf(this.visa, '%i');
+		switch rem(i, 2)
+		case 0
+			 this.par.timeconst = 1;
+		case 1 
+			 this.par.timeconst = 3;
+		end;
 
-    this.par.timeconst = this.par.timeconst * 10^(fix(i/2)-5);
+		this.par.timeconst = this.par.timeconst * 10^(fix(i/2)-5);
 
-    fprintf(this.visa, 'OFSL?\n');
-    this.par.slope = 6 * fscanf(this.visa, '%i') + 6;
+		fprintf(this.visa, 'OFSL?\n');
+		this.par.slope = 6 * fscanf(this.visa, '%i') + 6;
 
-    fprintf(this.visa, 'SYNC?\n');
-    this.par.sync = fscanf(this.visa, '%i');
+		fprintf(this.visa, 'SYNC?\n');
+		this.par.sync = fscanf(this.visa, '%i');
 
-    fprintf(this.visa, 'SENS?\n');
-    this.par.sensitivity = fscanf(this.visa,'%i');
-catch ME
-    fprintf('SR830.getparams: error, exiting\n');
-    this.close();
-    rethrow(ME);
+		fprintf(this.visa, 'SENS?\n');
+		this.par.sensitivity = fscanf(this.visa,'%i');
+													   
+	catch ME
+		fprintf('SR830.getparams: error, exiting\n');
+		this.close();
+		rethrow(ME);
+	end
 end
+
+function freqOutput = getFreq(this) % reads frequency only
+    try
+		fprintf(this.visa, 'FREQ?\n');
+		this.par.freq = fscanf(this.visa, '%f');
+	catch ME
+		fprintf('SR830.getFreq: error, exiting\n');
+		this.close();
+		rethrow(ME);
+    end
+    freqOutput = this.par.freq; 
 end
+
+function setPhas(this, phasVal)
+	try
+		fprintf(this.visa, 'PHAS\n');
+		this.par.phase = phasVal;
+		
+	catch ME
+		fprintf('SR830.setPhas: error, exiting\n');
+		this.close();
+		rethrow(ME);
+	end
+end
+
+function setFMod(this, fModVal)
+	try
+		fprintf(this.visa, 'FMOD\n');
+		this.par.internal_ref = fModVal;
+		
+	catch ME
+		fprintf('SR830.setFMod: error, exiting\n');
+		this.close();
+		rethrow(ME);
+	end
+end
+
+function setFreq(this, freqVal)
+	try
+		fprintf(this.visa, ['FREQ ', num2str(freqVal), '\n']);
+		this.par.freq = freqVal;
+		
+	catch ME
+		fprintf('SR830.setFreq: error, exiting\n');
+		this.close();
+		rethrow(ME);
+	end
+end
+
+function setAmp(this, ampVal)
+	try
+		fprintf(this.visa, 'SLVL\n');
+		this.par.vOutRMS = ampVal;
+		
+	catch ME
+		fprintf('SR830.setAmp: error, exiting\n');
+		this.close();
+		rethrow(ME);
+	end
+end
+
+function setSens(this, sens)
+    %level integer and their meanings:
+    %lookup = [00, 002e-9;...   % nV or fA
+    %          01, 005e-9;...   
+    %          02, 010e-9;...  
+    %          03, 020e-9;...  
+    %          04, 050e-9;...
+    %          05, 100e-9;...
+    %          06, 200e-9;...
+    %          07, 500e-9;...
+    %          08, 001e-6;...
+    %          09, 002e-6;...
+    %          10, 005e-6;...
+    %          11, 010e-6;...
+    %          12, 020e-6;...
+    %          13, 050e-6;...
+    %          14, 100e-6;...
+    %          15, 200e-6;...
+    %          16, 500e-6;...
+    %          17, 001e-3;...
+    %          18, 002e-3;...
+    %          19, 005e-3;...
+    %          20, 010e-3;...
+    %          21, 020e-3;...
+    %          22, 050e-3;...
+    %          23, 100e-3;...
+    %          24, 200e-3;...
+    %          25, 500e-3;...
+    %          26, 1;...
+    %           ];
+    %   index = 0;
+%   for i = 1:size(lookup)(1)
+%       if(sens <= lookup(i,2))
+%           index = lookup(i,1);
+%           break;
+%       end
+%   end
+%     try
+%         fprintf(this.visa, ['SENS ',num2str(index),'\n']);
+%     catch ME
+%         fprintf('SR830.sensitivity: Cannot write to SR830?\n')
+%         this.close();
+%         rethrow(ME);
+%     end
+end
+% 
+% function setTimeConstant(this, time)
+%     lookup = [00, 010e-6; ... 
+%               01, 030e-6; ... 
+%               02, 100e-6; ... 
+%               03, 300e-6; ... 
+%               04, 001e-3; ... 
+%               05, 003e-3; ... 
+%               06, 010e-3; ... 
+%               07, 030e-3; ... 
+%               08, 100e-3; ... 
+%               09, 300e-3; ... 
+%               10, 001e-0; ... 
+%               11, 003e-0; ... 
+%               12, 010e-0; ... 
+%               13, 030e-0; ... 
+%               14, 100e-0; ... 
+%               15, 300e-0; ... 
+%               16, 001e+3; ... 
+%               17, 003e+3; ... 
+%               18, 010e+3; ... 
+%               19, 030e+3; ... 
+%               ];
+%   index = 0;
+%   for i = 1:size(lookup)(1)
+%       if(time <= lookup(i,2))
+%           index = lookup(i,1);
+%           break;
+%       end
+%   end
+%
+%   try
+%       fprintf(this.visa, ['OFLT ',num2str(index),'\n']);
+%   catch ME
+%       fprintf('SR830.setTimeConstant: Cannot write to SR830?\n')
+%       this.close();
+%       rethrow(ME);
+%   end
+%               
+% end
+
 
 end % } END methods
 
