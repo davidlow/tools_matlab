70110c95f858e2ca72498636be43ae404b0e7847
Squidward <dhl88@cornell.edu>
2015-09-18 18:52:05 -0400

    DL: [NIdaq] Automatic LoggableObj Commit
    quick scan to tune the amplitude of squid I

--- measurement_scripts/SQUID_IV.m
+++ measurement_scripts/SQUID_IV.m
@@ -19,15 +19,15 @@ nidaq.p.gain        = 500;
 nidaq.p.lpf0        = 10;
 nidaq.p.mod_curr    = 0;
 nidaq.p.mod_biasr   = 2.5e3;
-nidaq.p.rate        = 2;
+nidaq.p.rate        = 5;
 nidaq.p.range       = 5; % options: 0.1, 0.2, 0.5, 1, 5, 10
-nidaq.p.src_amp     = .2;
-nidaq.p.src_numpts  = 400;
+nidaq.p.src_amp     = .15;
+nidaq.p.src_numpts  = 100;
 nidaq.p.squid_biasr = 2.5e3 + 3e3; %1.0k + 1.5k cold, 3k warm
 nidaq.p.T           = 4.28;
 nidaq.p.Terr        = .013;
 
-nidaq.notes = 'very long scan to make sure number of points, not rate';
+nidaq.notes = 'quick scan to tune the amplitude of squid I';
 
 %% Setup scan
 nidaq.setrate(nidaq.p.rate);
