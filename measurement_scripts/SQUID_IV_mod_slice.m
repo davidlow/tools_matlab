1b92c11684f2949c17b25e074f4326d8528f04e8
Squidward <dhl88@cornell.edu>
2015-09-22 18:10:27 -0400

    DL: [NIdaq] Automatic LoggableObj Commit
    Testing code: SQUID_IV_mod_slice for scanning a current slice

--- measurement_scripts/SQUID_IV_mod_slice.m
+++ measurement_scripts/SQUID_IV_mod_slice.m
@@ -37,7 +37,7 @@ nidaq.p.squid_biasr = 2.5e3 + 3e3; %1.0k + 1.5k cold, 3k warm
 nidaq.p.src_numpts  = nidaq.p.mod_curr * 2 / nidaq.p.mod_step;
 nidaq.p.range       = 5; % options: 0.1, 0.2, 0.5, 1, 5, 10
 
-nidaq.notes = 'code looks good.  Running test run of the long one!';
+nidaq.notes = 'Testing code: SQUID_IV_mod_slice for scanning a current slice';
 
 %% Setup scan
 
@@ -65,18 +65,18 @@ nidaq.setoutputdata(1,desout{2});
 
 %% Plot
 hold on
-plot(desout{1}/nidaq.p.squid_biasr*1e6, data(:,1));
+plot(desout{2}/nidaq.p.mod_biasr*1e6, data(:,1));
 
 title({['param = ', CSUtils.parsefnameplot(nidaq.lastparamsave)], ...
        ['data  = ', CSUtils.parsefnameplot(nidaq.lastdatasave)],  ...
        ['gain=',           num2str(nidaq.p.gain),                 ...
        ', lp f_0 =',      num2str(nidaq.p.lpf0),                 ...
-       ', hz, rate =',    num2str(nidaq.p.rate),                 ...
-       ', hz r_{bias} = ' num2str(nidaq.p.squid_biasr),            ...
+       ' hz, rate =',    num2str(nidaq.p.rate),                 ...
+       ' hz r_{bias} = ' num2str(nidaq.p.squid_biasr),            ...
        ', T = '           num2str(nidaq.p.T)                     ...
        ]});
-xlabel('I_{bias} = V_{bias}/R_{bias} (\mu A)','fontsize',20);
-ylabel('V_{mod} (V)','fontsize',20);
+xlabel('I_{mod} = V_{mod}/R_{mod} (\mu A)','fontsize',20);
+ylabel('V_{squid} (V)','fontsize',20);
 
 legendstr =  cell(1, length(nidaq.p.mod_curr));
 for i = 1:length(nidaq.p.mod_curr)
