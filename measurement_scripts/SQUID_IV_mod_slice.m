20d4d48058e033871284c621b9c3c3218d8bc78b
Squidward <dhl88@cornell.edu>
2015-09-22 18:07:08 -0400

    DL: [NIdaq] Automatic LoggableObj Commit
    code looks good.  Running test run of the long one!

--- measurement_scripts/SQUID_IV_mod_slice.m
+++ measurement_scripts/SQUID_IV_mod_slice.m
@@ -1,8 +1,7 @@
-% Sweeps through current biasing squid AND applies different mod coil
-% currents
+% Sits at constant current and sweeps mod coil current
 
 % change log
-% 15 09 22: davidlow runs for long scan, works
+% 15 09 22: davidlow create
 
 
 %% Initialize
@@ -23,58 +22,51 @@ nidaq = NIdaq('DL', 'Z:/data/montana_b69/Squid_Tests/150918/'); %save path
 % Add and set parameters here! not in the code! if you want more params
 % add them here  All of these 'should' be saved ;)
 nidaq.p.gain        = 500;
-nidaq.p.lpf0        = .3;
-nidaq.p.mod_curr    = linspace(0,77e-6,7);
-nidaq.p.mod_biasr   = 2.5e3 + 10e3; %1.0 + 1.5 cold, 10k warm
-nidaq.p.rate        = .1; %0.1 < rate < 2 857 142.9
-nidaq.p.range       = 5; % options: 0.1, 0.2, 0.5, 1, 5, 10
-nidaq.p.src_amp_I   = 30e-6; % current in amps
-nidaq.p.src_numpts  = 1000;
-nidaq.p.squid_biasr = 2.5e3 + 3e3; %1.0k + 1.5k cold, 3k warm
+nidaq.p.lpf0        = 300;
+nidaq.p.rate        = 100; %0.1 < rate < 2 857 142.9
 nidaq.p.T           = 4.38;
 nidaq.p.Terr        = .013;
-nidaq.p.scantime    = 0;
+
+nidaq.p.mod_curr    = 2*(77e-6); %max and min
+nidaq.p.mod_step    = 0.1e-6;
+nidaq.p.mod_biasr   = 2.5e3 + 10e3; %1.0 + 1.5 cold, 10k warm
+
+nidaq.p.src_amp_I   = 30e-6; % current in amps
+nidaq.p.squid_biasr = 2.5e3 + 3e3; %1.0k + 1.5k cold, 3k warm
+
+nidaq.p.src_numpts  = nidaq.p.mod_curr * 2 / nidaq.p.mod_step;
+nidaq.p.range       = 5; % options: 0.1, 0.2, 0.5, 1, 5, 10
 
 nidaq.notes = 'code looks good.  Running test run of the long one!';
 
 %% Setup scan
 
 nidaq.addinput_A ('Dev1', 0, 'Voltage', nidaq.p.range, 'SQUID V (sense)');
-nidaq.addinput_A ('Dev1', 4, 'Voltage', nidaq.p.range, 'unused');
+nidaq.addinput_A ('Dev1', 4, 'Voltage', nidaq.p.range, 'Unused');
 nidaq.addoutput_A('Dev1', 0, 'Voltage', nidaq.p.range, 'SQUID I (source)');
-nidaq.addoutput_A('Dev1', 1, 'Voltage', nidaq.p.range, 'unused');
+nidaq.addoutput_A('Dev1', 1, 'Voltage', nidaq.p.range, 'MOD I (source)');
 
 nidaq.setrate    (nidaq.p.rate);
 
 
 %%
-alldata   = cell(length(nidaq.p.mod_curr));
-alldesout = cell(length(nidaq.p.mod_curr));
-i = 1;
-for modcurr = nidaq.p.mod_curr
-    %% Setup data
-    desout = {nidaq.p.src_amp_I * nidaq.p.squid_biasr * sin(linspace(0,2*pi,nidaq.p.src_numpts)),...
-          modcurr * nidaq.p.mod_biasr *    linspace(1,1   ,nidaq.p.src_numpts)  ...
-         };
-    nidaq.setoutputdata(0,desout{1});
-    nidaq.setoutputdata(1,desout{2});
 
-    %% Run / collect data
-    [data, time] = nidaq.run();
-    alldata{i}   = data;
-    alldesout{i} = desout;
-    i = i+1;
-end
+
+%% Setup data
+desout = {nidaq.p.src_amp_I * nidaq.p.squid_biasr * linspace( 1,1, nidaq.p.src_numpts),...
+          nidaq.p.mod_curr  * nidaq.p.mod_biasr   * linspace(-1,1, nidaq.p.src_numpts) ...
+         };
+nidaq.setoutputdata(0,desout{1});
+nidaq.setoutputdata(1,desout{2});
+
+%% Run / collect data
+[data, time] = nidaq.run();
 
 
 %% Plot
 hold on
+plot(desout{1}/nidaq.p.squid_biasr*1e6, data(:,1));
 
-for i = 1:length(alldata)
-    data   = alldata{i};
-    desout = alldesout{i};
-    plot(desout{1}/nidaq.p.squid_biasr*1e6, data(:,1));
-end
 title({['param = ', CSUtils.parsefnameplot(nidaq.lastparamsave)], ...
        ['data  = ', CSUtils.parsefnameplot(nidaq.lastdatasave)],  ...
        ['gain=',           num2str(nidaq.p.gain),                 ...
