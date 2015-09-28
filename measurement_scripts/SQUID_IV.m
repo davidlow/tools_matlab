40a4d2bf7683b8e62050be970f53f32f4e26cb63
Squidward <dhl88@cornell.edu>
2015-09-28 09:41:24 -0400

    DL: [NIdaq] Automatic LoggableObj Commit
    5 hz scan because last scan had mild hysteresis and I am impatient

--- measurement_scripts/SQUID_IV.m
+++ measurement_scripts/SQUID_IV.m
@@ -16,19 +16,19 @@ nidaq = NIdaq('DL', 'Z:/data/montana_b69/Squid_Tests/150928/'); %save path
 % Add and set parameters here! not in the code! if you want more params
 % add them here  All of these 'should' be saved ;)
 nidaq.p.gain        = 500;
-nidaq.p.lpf0        = 3;
+nidaq.p.lpf0        = 10;
 nidaq.p.mod_curr    = 0;
 nidaq.p.mod_biasr   = 2.5e3;
-nidaq.p.rate        = 1; %0.1 < rate < 2 857 142.9
+nidaq.p.rate        = 5; %0.1 < rate < 2 857 142.9
 nidaq.p.range       = 10; % options: 0.1, 0.2, 0.5, 1, 5, 10
 nidaq.p.src_amp_I   = 20e-6; % current in amps
-nidaq.p.src_numpts  = 100;
+nidaq.p.src_numpts  = 1000;
 nidaq.p.squid_biasr = 2.5e3 + 3e3; %1.0k + 1.5k cold, 3k warm
 nidaq.p.T           = 4.3;
 nidaq.p.Terr        = .013;
 nidaq.p.scantime    = 0;
 
-nidaq.notes = 'Scanning at 3hz to see how bad hysteresis';
+nidaq.notes = '5 hz scan because last scan had mild hysteresis and I am impatient';
 
 %% Setup scan
 
